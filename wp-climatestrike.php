7cb489bff5925c0a1b47113a73fc7864d0129efd
Tom <webrocker@gmail.com>
2019-09-03 11:29:41 +0200

    Clear caches on strikeday. fixes #3

--- wp-climatestrike.php
+++ wp-climatestrike.php
@@ -5,7 +5,7 @@
 	 * Description: A small plugin to put your WordPress site on climate strike
 	 * Author: Tom Arnold
 	 * Author URI: https://www.webrocker.de/
-	 * Version: 1.1.2
+	 * Version: 1.1.3
 	 */
 	function wbr_wpcs_redirect() {
 
@@ -14,7 +14,7 @@
 
 		$home_url = home_url();
 		$privacy_page_url = get_privacy_policy_url();
-		$privacy_page_rel_url = substr($privacy_page_url,strlen($home_url),-1) .'/';
+		$privacy_page_rel_url = trailingslashit(substr($privacy_page_url,strlen($home_url),-1));
 
 		// @@TODO: make this from plugin settings:
 		$allowed_urls = array('/wp-admin/','/impressum/',$privacy_page_rel_url);
@@ -30,6 +30,10 @@
 		if ( is_admin() || in_array($current_url,$allowed_urls)) {
 			return;
 		} else if ($today == $strike) {
+			wp_cache_flush();
+			if (function_exists('wpsc_delete_files')) {
+				wpsc_delete_files(get_supercache_dir() . $current_url);
+			}
 			status_header(503);
 			include $strike_path;
 			exit;
@@ -37,6 +41,4 @@
 
 	}
 	add_action( 'template_redirect', 'wbr_wpcs_redirect');
-
-
 ?>
