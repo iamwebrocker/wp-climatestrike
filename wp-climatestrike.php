fcd969d88f9bdbe88273d9ae48e10cb43d19d0e4
Tom <webrocker@gmail.com>
2019-09-03 10:43:48 +0200

    Add better HTTP status (503), fixes #2 (thanks @sebastiangreger)

--- wp-climatestrike.php
+++ wp-climatestrike.php
@@ -5,7 +5,7 @@
 	 * Description: A small plugin to put your WordPress site on climate strike
 	 * Author: Tom Arnold
 	 * Author URI: https://www.webrocker.de/
-	 * Version: 1.1.1
+	 * Version: 1.1.2
 	 */
 	function wbr_wpcs_redirect() {
 
@@ -19,18 +19,19 @@
 		// @@TODO: make this from plugin settings:
 		$allowed_urls = array('/wp-admin/','/impressum/',$privacy_page_rel_url);
 		$strike_page = 'climatestrike.html';
-		$current_url = $_SERVER['REQUEST_URI'];
+		$current_url = trailingslashit($_SERVER['REQUEST_URI']);
 
 		if (is_file(get_stylesheet_directory() . '/' . $strike_page )) {
-			$strike_url = get_stylesheet_directory_uri() . '/' . $strike_page;
+			$strike_path = get_stylesheet_directory() . '/' . $strike_page;
 		} else {
-			$strike_url = plugins_url('templates/' . $strike_page,__FILE__);
+			$strike_path = plugin_path(__FILE_) . '/' . $strike_page;
 		}
 
 		if ( is_admin() || in_array($current_url,$allowed_urls)) {
 			return;
 		} else if ($today == $strike) {
-			wp_redirect($strike_url);
+			status_header(503);
+			include $strike_path;
 			exit;
 		}
 
